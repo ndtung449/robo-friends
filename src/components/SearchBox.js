7259a44b3a2912a0b1134f80f60a3fc35c98fceb
tung.nguyen <tung.nguyen@2clicksolutions.com>
2018-09-05 08:45:54 +0700

    optimize as suggested by lighthouse

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const SearchBox = ({ handleSearchChange }) => (
   <input
+    aria-label='Search Robot'
     className='pa3 ba b--green bg-lightest-blue'
     placeholder='filter by name'
     onChange={handleSearchChange}
